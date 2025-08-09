7cfde797697e2e4bcecc36677e2a0ab0422502af
acsandmann <157552025+acsandmann@users.noreply.github.com>
2025-08-08 23:25:30 -0400

    fix: ax prompting

--- src/AccessibilityManager.swift
+++ src/AccessibilityManager.swift
@@ -1,20 +1,11 @@
 import Cocoa
-import CoreFoundation
 
 enum AccessibilityManager {
-	private static let didPromptKey = "AccessibilityManagerDidPrompt"
-
 	static func ensurePermissions() {
-		guard !AXIsProcessTrusted() else { return }
-
-		let defaults = UserDefaults.standard
-		if !defaults.bool(forKey: didPromptKey) {
-			AXIsProcessTrustedWithOptions(
-				[
-					kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String: true,
-				] as CFDictionary)
-			defaults.set(true, forKey: didPromptKey)
-		}
+		guard
+			!AXIsProcessTrustedWithOptions(
+				[kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String: true] as CFDictionary)
+		else { return }
 
 		print("Waiting for accessibility permissions...")
 		Timer.scheduledTimer(withTimeInterval: 2.5, repeats: true) { timer in
